a059867118747545d6816500071b75ffc3a11395
Rhome Falaize <rhome.falaize@gmail.com>
2018-07-19 22:33:36 -0400

    Add cpp solver

--- SolverCpp/SolverCpp/SolverCpp.cpp
+++ SolverCpp/SolverCpp/SolverCpp.cpp
@@ -1,13 +1,10 @@
 #include "stdafx.h"
 #include "ortools/linear_solver/linear_solver.h"
 #include "ortools/linear_solver/linear_solver.pb.h"
-#include <iostream>
-
-using namespace std;
 
 namespace operations_research {
-	void RunTest(MPSolver::OptimizationProblemType optimization_problem_type)
-	{
+	void RunTest(
+		MPSolver::OptimizationProblemType optimization_problem_type) {
 		MPSolver solver("Glop", optimization_problem_type);
 		MPVariable* const x = solver.MakeNumVar(0.0, 1, "x");
 		MPVariable* const y = solver.MakeNumVar(0.0, 2, "y");
@@ -22,10 +19,7 @@ namespace operations_research {
 	}
 
 	void RunExample() {
-		printf("Start program...");
 		RunTest(MPSolver::GLOP_LINEAR_PROGRAMMING);
-		printf("Press any key to return... ");
-		cin;
 	}
 }
 
@@ -33,4 +27,3 @@ int main(int argc, char** argv) {
 	operations_research::RunExample();
 	return 0;
 }
-
